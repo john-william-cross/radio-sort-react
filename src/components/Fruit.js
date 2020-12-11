33891304a490cdb4e0288aa56697248be30c94be
John Cross <john.william.cross@gmail.com>
2020-12-11 09:10:48 -0800

    import date-fns library

--- src/components/Fruit.js
+++ src/components/Fruit.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { addExclamationPoint } from "../utils/helpers";
+import format from "date-fns/format";
 
 export default function Fruit(props) {
    console.log("The props we passed from the parent component: ", props);
    return (
-      <h1>
-         {props.fruit.name}
-         {/* example of adding a function */}
-         {addExclamationPoint()}
-      </h1>
+      <div>
+         <h1>
+            {props.fruit.name}
+            {/* example of adding a function */}
+            {addExclamationPoint()}
+         </h1>
+         <p>{format(props.fruit.createdAt, "MMM. d, yyyy")}</p>
+      </div>
    );
 }
--- src/data/fruits.js
+++ src/data/fruits.js
@@ -3,10 +3,12 @@ export const fruits = [
       name: "Apple",
       rating: 4,
       id: "bbb6c2fe-3357-4e68-82c7-48dfd70757f0",
+      createdAt: 1607587200000,
    },
    {
       name: "Banana",
       rating: 5,
       id: "5582e9cd-f1eb-4b90-a94f-58c30bf060e3",
+      createdAt: 1607705153861,
    },
 ];
